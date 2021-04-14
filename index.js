854a3fba391234d5009b32f4db32e7509170ae86
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2021-04-14 20:07:01 +0100

    learn how to read and write file in node.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const message = "Hello World"
-console.log(message) 
+const fs = require('fs')
+
+const readMessage = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(readMessage)
+
+const replymessage = `This is the information we could find about avocade: ${readMessage}\nCreated on ${new Date()}`
+fs.writeFileSync('./txt/ouput.txt', replymessage)
+console.log("Job Done")
