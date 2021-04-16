b27afa141598ce140e83e79be5475632d6629e5a
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2021-04-16 08:48:33 +0100

    practices about routing

--- index.js
+++ index.js
@@ -2,36 +2,45 @@
 //Reading method
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 ////////////////////////////////////////
 ///////For Files
-const readEncryptedMessage = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(readEncryptedMessage)
+// const readEncryptedMessage = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(readEncryptedMessage)
 
-//writing method
-const ForwardMessage = `This is the result for your request ${readEncryptedMessage}\nCreated on ${new Date ()}`
-fs.writeFileSync('./txt/output1.txt', ForwardMessage)
-console.log("Done")
+// //writing method
+// const ForwardMessage = `This is the result for your request ${readEncryptedMessage}\nCreated on ${new Date ()}`
+// fs.writeFileSync('./txt/output1.txt', ForwardMessage)
+// console.log("Done")
 
-//reading and writing files asynchronously
-fs.readFile('./txt/startt.txt', 'utf-8', (err, data1) => {
-    if (err) return console.log('ERROR!')
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2)
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3)
+// //reading and writing files asynchronously
+// fs.readFile('./txt/startt.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('ERROR!')
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2)
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3)
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log("it has been uploaded")
-            })
-        })
-    })
-})
-console.log("I will get here before you")
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log("it has been uploaded")
+//             })
+//         })
+//     })
+// })
+// console.log("I will get here before you")
 ///////////////////////////////////////////////////////////
 ///For server
 
 const server = http.createServer(function(req, res){
-    res.end('Hello from the serverside here o')
+    const pathName = req.url;
+    if (pathName ==='/' || pathName === '/overview'){
+        res.end('this is the OVERVIEW PAGE')
+    }else if(pathName === '/product'){
+        res.end('this is the PRODUCT PAGE')
+    }else{
+        res.writeHead(404)
+        res.end('Page not found!')
+    }
 })
 
 server.listen(8000, '127.0.0.1', function(){
